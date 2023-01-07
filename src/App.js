11e9e3dba6f4e1b7fd6e174dff1911f416cdb332
Shafayet-Shawal96 <shafayet.shawal@gmail.com>
2023-01-07 19:32:54 +0600

    new update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline text-primary">
         Hello world!
       </h1>
-      <h3 className="font-caveat text-background">hjgjhfkfghf</h3>
+      <h3 className="font-caveat text-background">shawal</h3>
     </div>
   );
 }
