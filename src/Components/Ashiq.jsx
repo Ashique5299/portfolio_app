4744a204b13aaafd63c7fe5ef09c94ea0f2b9fe5
ashique5299 <ashique5299@gmail.com>
2023-01-07 21:03:44 +0600

    portfolio header

--- src/Components/Ashiq.jsx
+++ src/Components/Ashiq.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Ashiq() {
-  return <div>Ashiq qqqqqqqqqqqqqqqq</div>;
+  return (
+    <div>
+      <h1 className="Portfolio_header text-center text-5xl font-semibold">
+        PORTFOLIO
+      </h1>
+      <h3 className="font-jost text-center  ">
+        <span className="text-2xl text-primary">my</span>
+        <span className="font-caveat text-4xl ml-2">Cases</span>
+      </h3>
+    </div>
+  );
 }
 
 export default Ashiq;
